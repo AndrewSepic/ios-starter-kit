e7acbdabe6be672eade4cb976a6f176e7ffd9ef2
Andrew Sepic <andrew.sepic@mapbox.com>
2024-11-08 13:31:13 -0500

    Added MapboxMaps, accessToken & initial ContentView

--- Starter Kit/ContentView.swift
+++ Starter Kit/ContentView.swift
@@ -6,18 +6,26 @@
 //
 
 import SwiftUI
+// The API may change in future releases.
+@_spi(Experimental) import MapboxMaps
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        let center = CLLocationCoordinate2D(
+            latitude: 42.34942,
+            longitude: -71.06403
+          )
+        // Attempt to create a StyleURI from the raw value, with a default fallback
+        let styleURI = StyleURI(rawValue: "mapbox://styles/examples/cm37hh1nx017n01qk2hngebzt") ?? .streets
+        
+        return Map(initialViewport: .camera(
+            center: center,
+            zoom: 10.5
+            )).mapStyle(MapStyle(uri: styleURI))
     }
 }
+
 
 #Preview {
     ContentView()
